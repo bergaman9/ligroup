3b75a85ded14cef4ff94312d0dae902e7fa6a822
Ömer Güler <omerguler53@gmail.com>
2025-03-13 12:26:32 +0300

    new commit

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ io.on("connection", (socket) => {
 });
 
 app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"));
 app.use(
   bodyParser.urlencoded({
